0dbfcb49187ec04dfff76876a2faa3309113ac99
JHyeok <dev.jhyeok@gmail.com>
2020-03-23 11:36:41 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## ❯ Express TypeScript Starter
+
+The most basic Express server using TypeScript
+
+### Features
+
+- TypeScript language
+- The most primitive with no collateral
+- Logging with Sentry, logging with File System and Consolse
+- Maintain code style with eslint and prettierrc
+
+### Environment Variable Management
+
+```env
+# APPLICATION
+PORT=3000
+
+# Sentry
+SENTRY_DSN=
+```
+
+### Build Setup
+
+Steps to run this project:
+
+1. Run `yarn install` command
+2. Run `yarn dev` command
+3. Run `yarn start` command
---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { logger } from "./utils/Logger";
+import { useSentry } from "./utils/Sentry";
+import cors from "cors";
+import helmet from "helmet";
+
+export class App {
+    public app: express.Application;
+
+    constructor() {
+        this.app = express();
+        this.setMiddlewares();
+    }
+
+    private setMiddlewares(): void {
+        this.app.use(cors());
+        this.app.use(helmet());
+    }
+
+    public async createExpressServer(port: number) {
+        try {
+            useSentry(this.app);
+
+            this.app.listen(port, () => {
+                logger.info(`Server is running on http://localhost:${port}`);
+            });
+        } catch (error) {
+            logger.error(error);
+        }
+    }
+}
