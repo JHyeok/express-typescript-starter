582c1f72caa56551c2f5960a3637e55a707be218
JHyeok <dev.jhyeok@gmail.com>
2020-05-30 11:51:10 +0900

    feat: API Router 추가

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import { env } from "./env";
 import { logger } from "./utils/Logger";
 import { useSentry } from "./utils/Sentry";
 import cors from "cors";
 import helmet from "helmet";
+import routes from "./api";
 
 export class App {
   public app: express.Application;
@@ -10,11 +12,17 @@ export class App {
   constructor() {
     this.app = express();
     this.setMiddlewares();
+    this.setRouter();
   }
 
   private setMiddlewares(): void {
     this.app.use(cors());
     this.app.use(helmet());
+  }
+
+  private setRouter(): void {
+    this.app.get("/", (req, res) => res.send("OK"));
+    this.app.use(env.app.apiPrefix, routes());
   }
 
   public async createExpressServer(port: number) {
